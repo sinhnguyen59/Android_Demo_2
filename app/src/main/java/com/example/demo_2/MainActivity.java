8c451be6a36d2d7447791b4694b23e987e5bd365
Sinh Nguyen <snguyen59@aceproject.space>
2022-03-11 09:42:11 -0600

    Add lifecycle methods in MainActivity

--- app/src/main/java/com/example/demo_2/MainActivity.java
+++ app/src/main/java/com/example/demo_2/MainActivity.java
@@ -54,6 +54,26 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
     public void signIn() {
         Intent signInIntent = gsc.getSignInIntent();
         startActivityForResult(signInIntent, 1000);
